d63bf9271393b01d11bb246566c7eb1dd3b72ad3
Sunny Jadhav <113296063+jadhavsunny23@users.noreply.github.com>
2025-10-18 10:13:06 +0530

    Task-2 projects is added

---
+++ TASK-2/User-Form/script.js
@@ -0,0 +1,39 @@
+function formValidate() {
+    let first = document.forms["mForm"]["ufname"].value;
+    let last = document.forms["mForm"]["ulname"].value;
+    let age = document.forms["mForm"]["uage"].value;
+    let mobile = document.forms["mForm"]["unumber"].value;
+    let mail = document.forms["mForm"]["uemail"].value;
+
+    if(first, last, age, mobile, mail == "") {
+        alert("All Fields are mandatory/required.");
+        return false;
+    }
+
+    if(first== "") {
+        alert("All Fields are mandatory. Please enter First name.");
+        return false;
+    }
+    if(last == "") {
+        alert("All Fields are mandatory. Please enter Last name.");
+        return false;
+    }
+    if(age== "") {
+        alert("All Fields are mandatory. Please Enter Age");
+        return false;
+    }
+    if(mobile== "") {
+        alert("All Fields are mandatory. Please enter Mobile No.");
+        return false;
+    }
+    if(mail == "") {
+        alert("All Fields are mandatory. Please enter Email.");
+        return false;
+    }
+ 
+    
+    
+}
+function onClick() {
+        alert("User Data is Submitted.");
+    }
